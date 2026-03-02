27fbe8303fe51d74123ce3d9dace7ad3cb08a643
shivika0309 <shivikabisht.0318@gmail.com>
2026-03-03 02:37:05 +0530

    Create ZeroesToEnd.java

---
+++ LeetCode/ZeroesToEnd.java
@@ -0,0 +1,15 @@
+class ZeroesToEnd {
+    public void moveZeroes(int[] nums) {
+           int left=0,right=0; 
+           for( right=0;right<nums.length;right++){
+            
+            if(nums[right]!=0){
+                int temp=nums[right];
+                nums[right]=nums[left];
+                nums[left]=temp;
+                left++;
+            }
+           }
+     } 
+}//help
+    
