7f2e2f3029c9f7b56f89430aed4d6e6579387fcb
shivika0309 <shivikabisht.0318@gmail.com>
2026-03-05 01:53:22 +0530

    Create GoalsOfVictory.java

---
+++ CodeForces /Rating-800CfSheet31/GoalsOfVictory.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class GoalsOfVictory {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        
+        for(int s=0;s<t;s++){
+            int sum=0;
+            int l=sc.nextInt();
+            int[] arr=new int[l-1];
+            for(int i=0;i<l-1;i++){  //input of array elements
+                arr[i]=sc.nextInt();
+                sum+=arr[i];
+            }
+
+            System.out.println(-1*sum);
+        }
+    }
+}
