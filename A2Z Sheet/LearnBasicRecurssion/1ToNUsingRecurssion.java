dbb1edc90ac901d6e09d333303af3f9dc64f397e
shivika0309 <shivikabisht.0318@gmail.com>
2026-07-08 09:11:56 +0530

    Add recursive method to print numbers from 1 to N

---
+++ A2Z Sheet/LearnBasicRecurssion/1ToNUsingRecurssion.java
@@ -0,0 +1,9 @@
+class Solution {
+    public void printNumbers(int n) {
+        // Your code goes here
+        if(n<1)
+        return;
+        printNumbers(n-1);
+        System.out.println(n);
+    }
+}
