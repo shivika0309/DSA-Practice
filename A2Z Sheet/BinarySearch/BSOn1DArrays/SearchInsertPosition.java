627be243ebb120c6c2b83e7b03b196e55f88da29
shivika0309 <shivikabisht.0318@gmail.com>
2026-07-09 20:43:45 +0530

    Add SearchInsertPosition class with upperBound method

---
+++ A2Z Sheet/BinarySearch/BSOn1DArrays/SearchInsertPosition.java
@@ -0,0 +1,21 @@
+class SearchInsertPosition {
+    public int upperBound(int[] nums, int x) {
+        int low=0;
+        int high=nums.length-1;
+        int ans=nums.length;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==x){
+                low=mid+1;
+            }
+            else if(nums[mid]>x){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+}
