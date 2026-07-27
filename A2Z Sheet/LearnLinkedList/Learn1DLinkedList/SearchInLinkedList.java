a04822bdaa16596269396e7e7dbfb69d5d85a1eb
shivika0309 <shivikabisht.0318@gmail.com>
2026-07-27 21:05:06 +0530

    Add searchKey method to SearchInLinkedList class
    Implement search functionality in a linked list.

---
+++ A2Z Sheet/LearnLinkedList/Learn1DLinkedList/SearchInLinkedList.java
@@ -0,0 +1,23 @@
+/* Defination of ListNoode
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int value) {
+        this.val = value;
+        this.next = null;
+    }
+}
+*/
+
+class SearchInLinkedList {
+    public boolean searchKey(ListNode head, int key) {
+        ListNode temp=head;
+        while(temp!=null){
+            if(temp.val==key)
+            return true;
+            temp=temp.next;
+        }
+        return false;
+    }
+}
